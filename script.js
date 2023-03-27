f26994a43476339bc6c181c8125c237fcf6058c7
dsl2022 <2015rpro@gmail.com>
2023-03-27 16:58:17 -0400

    updated

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('executeBtn').addEventListener('click', function() {
+    const outputElement = document.getElementById('output');
+
+    // Your JavaScript code goes here
+    const message = 'Hello, JavaScript!';
+    outputElement.textContent = message;
+});
